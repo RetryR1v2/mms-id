2a6592fbed0e9d8145cd4140bb651aad1793be53
Markus Mueller <147660115+RetryR1v2@users.noreply.github.com>
2025-01-18 14:42:22 +0100

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -2,7 +2,7 @@ fx_version 'cerulean'
 rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
 game 'rdr3'
 
-version '1.1.8'
+version '1.1.9'
 vorp_checker 'yes'
 vorp_name '^5MMS-ID ^4version Check^3'
 vorp_github 'https://github.com/RetryR1v2/mms-id'
